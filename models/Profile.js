5055fce8668e651a99c6a74435ec69f47e27d8cf
Rakib1415 <rakibiuaece@gmail.com>
2022-10-08 15:14:55 +0600

    feat:model created and validation

--- models/Profile.js
+++ models/Profile.js
@@ -11,6 +11,12 @@ const profileSchema = new Schema({
         minlength: [2, 'name is too short'],
         maxlength: [20, 'name is too long'],
     },
+    phoneNumber: {
+        type: String,
+        required: true,
+        minlength: [11, 'phone number is invalid'],
+        maxlength: [14, 'phone number is invalid'],
+    },
     avatar: String,
     user: {
         type: Schema.Types.ObjectId,
